cdce7818baedd704ed8ac4a858298bd68be83674
barld <bakeds@gmail.com>
2015-09-28 21:21:23 +0200

    add internship model

---
+++ src/Portfolio/Models/Internships.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class Internships : MarkDownFactory<Internship>
+    {
+        public Internships(string webRootPath) : base(webRootPath, "Internships/") { }
+        public override Internship Create(string fileName) => new Internship(fileName);
+    }
+
+    public class Internship : MarkDownModel
+    {
+        public Internship(string fileName) : base(fileName){ }
+    }
+}
