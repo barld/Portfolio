dc6d7e70c01dfcf8ff7cfbf0d474d37dadc0534b
barld <bakeds@gmail.com>
2015-09-28 13:40:14 +0200

    add angular js to frontpage

---
+++ src/Portfolio/Controllers/Api/ProjectController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Portfolio.Models;
+using Microsoft.AspNet.Hosting;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Portfolio.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+        public ProjectController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Project> Get()
+        {
+            var projects = new Projects(_environment.WebRootPath);
+
+            return projects.All;
+        }
+    }
+}
--- src/Portfolio/wwwroot/js/site.js
+++ src/Portfolio/wwwroot/js/site.js
@@ -1,1 +1,22 @@
 ﻿// Write your Javascript code.
+
+
+
+var ProjectConroller = function ($scope, $resource) {
+    var vm = this;
+
+    var project = $resource("/api/project", {}, {
+        query: { method: 'GET', isArray: true }
+    })
+
+    $scope.all = project.query();
+
+    vm.title = '';
+
+    activate();
+
+    function activate() { }
+}
+
+Portfolio = angular.module('Portfolio', ['ngResource'])
+Portfolio.controller('ProjectController', ['$scope', '$resource', ProjectConroller]);
